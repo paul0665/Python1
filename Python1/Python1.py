673a5aa5a9a168db401954214b286d7ae2c58d6e
Paul <47947919+paul0665@users.noreply.github.com>
2019-12-18 10:28:50 +0800

    Add files via upload

--- Python1/Python1.py
+++ Python1/Python1.py
@@ -1,6 +1,8 @@
 print("Hello World\n");
 object = ["String","String2","String3","String4"];
 print(object);
+print("\n");
+print(len(object));
 print("\ninsert in to object...\n");
 object.insert(5,"String5");
 object.insert(6,"String6");
