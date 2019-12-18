c491cabb81ec7c9d168e420a04e6ed7740e424b3
Paul <47947919+paul0665@users.noreply.github.com>
2019-12-18 10:20:52 +0800

    Add files via upload

---
+++ Python1/Python1.py
@@ -0,0 +1,9 @@
+print("Hello World\n");
+object = ["String","String2","String3","String4"];
+print(object);
+print("\ninsert in to object...\n");
+object.insert(5,"String5");
+object.insert(6,"String6");
+object.insert(7,"String7");
+print(object);
+
